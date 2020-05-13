84fb07e06fbc3bc29a4ba877eb8444f2af121949
charles <charlesclinton2003@gmail.com>
2020-05-13 14:45:24 +0100

    changed db to sqlite3

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -99,12 +99,8 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogapi',
-        'USER': 'postgres',
-        'PASSWORD': 'henry2014',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
